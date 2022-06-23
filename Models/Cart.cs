0afbeb8bbad8f0807255982194c7ec67020cc97d
Nhiem <nhiem858@gmail.com>
2022-06-23 12:21:12 +0700

    model

---
+++ Models/Cart.cs
@@ -0,0 +1,9 @@
+﻿namespace Computer_Store.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
---
+++ Models/CartItems.cs
@@ -0,0 +1,13 @@
+﻿namespace Computer_Store.Models
+{
+    public class CartItems
+    {
+        public int Id { get; set; }
+
+        public int ProductID { get; set; }
+
+        public int quantity { get; set; }
+
+        public int CartID { get; set; }
+    }
+}
